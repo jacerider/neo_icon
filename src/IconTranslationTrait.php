ec6445174726dec9ca980faf9803f66f4536f11b
JaceRider <jacerider@gmail.com>
2024-11-07 09:44:07 -0600

    Add admin icon trait.

--- src/IconTranslationTrait.php
+++ src/IconTranslationTrait.php
@@ -20,9 +20,27 @@ trait IconTranslationTrait {
    *   An array of prefixes.
    * @param bool $ignore_status
    *   If TRUE, the status will be ignored.
+   *
+   * @return \Drupal\neo_icon\IconElement
+   *   The icon element.
    */
-  protected function icon($text = NULL, $icon = NULL, $library = NULL, array $prefix = [], $ignore_status = FALSE) {
+  protected function icon($text = NULL, $icon = NULL, $library = NULL, array $prefix = [], $ignore_status = FALSE): IconElement {
     return new IconElement($text, $icon, $library, $prefix, $ignore_status);
+  }
+
+  /**
+   * Get an admin icon.
+   *
+   * @param string $text
+   *   The icon text.
+   * @param string $icon
+   *   The icon id.
+   *
+   * @return \Drupal\neo_icon\IconElement
+   *   The icon element.
+   */
+  protected function adminIcon($text = NULL, $icon = NULL): IconElement {
+    return $this->icon($text, $icon, NULL, ['admin']);
   }
 
 }
