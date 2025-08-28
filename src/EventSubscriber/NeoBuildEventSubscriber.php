bda7ac8d9e77d1289494609be851b11360cf932b
JaceRider <jacerider@gmail.com>
2025-08-28 10:48:56 -0500

    Use css variable.

--- src/EventSubscriber/NeoBuildEventSubscriber.php
+++ src/EventSubscriber/NeoBuildEventSubscriber.php
@@ -46,23 +46,9 @@ class NeoBuildEventSubscriber implements EventSubscriberInterface {
       }
     }
     if ($libraryIds) {
-      // $utilities = [
-      //   'icon-*' => [
-      //     '--tw-content' => '--value(--icon-*)',
-      //     'display' => 'inline-block',
-      //     'content' => 'var(--tw-content)',
-      //     'font-family' => '--value(--icon-library-*)',
-      //     '-webkit-font-smoothing' => 'antialiased',
-      //     '-moz-osx-font-smoothing' => 'grayscale',
-      //     'font-style' => 'normal',
-      //     'font-variant' => 'normal',
-      //     'font-weight' => 'normal',
-      //     'line-height' => 1,
-      //   ],
-      // ];
       $collection->addTailwindUtility('icon-*', [
         '--tw-content' => '--value(--icon-*)',
-        'display' => 'inline-block',
+        'display' => 'var(--icon-display, inline-block)',
         'content' => 'var(--tw-content)',
         'font-family' => '--value(--icon-library-*)',
         '-webkit-font-smoothing' => 'antialiased',
